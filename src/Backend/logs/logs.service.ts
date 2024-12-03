159378cd29f33c56698b2252faeb31ddae535f58
malekammarr <malekammar0@outlook.com>
2024-12-03 05:14:09 +0200

    Malek

---
+++ src/Backend/logs/logs.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Logs } from 'src/schemas/logs.schema';
+import { LogsModule } from './logs.module';
+
+@Injectable()
+export class LogsService {
+
+  constructor(
+    @InjectModel(Logs.name, 'eLearningDB')
+    private readonly LogsModel: Model<Logs>, // Inject the User model for DB operations
+  ){}
+
+ async create(email:string,pass:string): Promise<Logs> {
+    try{
+
+      const log = new this.LogsModel({
+        email,pass
+      })
+
+      // Save the user to the database
+      return await log.save();
+
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw new Error('Instructor registration failed');
+    }
+  }
+
+  async getLogs()
+  {
+    try{
+      const logs = await this.LogsModel.find();
+      return logs;
+    }
+    catch (error) {
+      console.error('Error fetching logs:', error);
+      throw new Error('Error fetching logs');
+    }
+  }
+  
+
+  }
+
+  
+
