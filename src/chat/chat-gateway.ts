00a38b220e9792e5bcfabdc9c6314b79798dfe1b
Yousssefgallal <youssef.galal05@gmail.com>
2024-12-08 00:27:52 +0200

    chat gateway Gee

--- src/chat/chat-gateway.ts
+++ src/chat/chat-gateway.ts
@@ -1,17 +1,34 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Socket, Server } from "socket.io";
 
 
-@WebSocketGateway(3002)
-export class ChatGateway {
+@WebSocketGateway(3002, {cors: {origin: '*'}})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     @WebSocketServer() server: Server;
+
+    handleConnection(client: Socket) {
+        console.log('New user Connected..', client.id)
+
+        client.broadcast.emit('user-joined', {
+            message: `New user joined the chat: ${client.id}`,
+        })
+    }
+
+    handleDisconnect(client: Socket) {
+        console.log('User Disonnected..', client.id)
+
+        this.server.emit('user-left', {
+            message: `User left the chat: ${client.id}`,
+        })
+    }
+
     @SubscribeMessage('newMessage')
     handleNewMessage(client: Socket, message: any){
         console.log(message)
 
         client.emit('Reply','Ahla mesa aalek ya Geee')
 
-        this.server.emit('Reply', '')
+        this.server.emit('Reply', 'Mesa Aal Kol')
     }
 }
