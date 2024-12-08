4ff50b0e5c8fd57580b4a9a1ca5aa6d080ade563
Boudyyyy <abdallahtarek2022@gmail.com>
2024-12-08 21:18:24 +0200

    Data Backup

--- src/Backend/backup/backup.controller.ts
+++ src/Backend/backup/backup.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Param, Delete } from '@nestjs/common';
 import { BackupService } from './backup.service';
 import { Backup } from 'src/schemas/backup.schema';
 
@@ -6,13 +6,27 @@ import { Backup } from 'src/schemas/backup.schema';
 export class BackupController {
   constructor(private readonly backupService: BackupService) {}
 
-  @Post()
-  async createBackup(@Body() backupData: Partial<Backup>): Promise<Backup> {
-    return this.backupService.createBackup(backupData);
+  // Get all backups
+  @Get()
+  async getBackups() : Promise<Backup[]> {
+    return this.backupService.getBackups();
   }
 
-  @Get()
-  async getAllBackups(): Promise<Backup[]> {
-    return this.backupService.getAllBackups();
+  // Get a specific backup by backupId
+  @Get(':backupId')
+  async getBackupById(@Param('backupId') backupId: string) {
+    return this.backupService.getBackupById(backupId);
+  }
+
+  // Create a new backup (manual)
+  @Post('create')
+  async createBackup() {
+    return this.backupService.createBackup();
+  }
+
+  // Delete a backup by backupId
+  @Delete(':backupId')
+  async deleteBackup(@Param('backupId') backupId: string) {
+    return this.backupService.deleteBackup(backupId);
   }
 }
